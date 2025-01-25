2cba9b88f99cdc668a334a5770da2bee11adf126
Kevin <kevin.yamauchi@gmail.com>
2025-01-25 16:44:20 +0100

    add function to convert to graph with skan

--- scripts/view_simple_t.py
+++ scripts/view_simple_t.py
@@ -3,9 +3,14 @@
 import napari
 
 from skeleplex.data.skeleton_image import simple_t
+from skeleplex.graph.image_to_graph import image_to_graph_skan
 
 # create the image
 image = simple_t()
+
+# make the graph
+summary_table = image_to_graph_skan(image)
+print(summary_table)
 
 viewer = napari.Viewer()
 viewer.add_image(image, name="simple T skeleton")
--- src/skeleplex/graph/image_to_graph.py
+++ src/skeleplex/graph/image_to_graph.py
@@ -1,1 +1,38 @@
 """Utilities to convert a skeleton image to a graph."""
+
+import networkx as nx
+import numpy as np
+from skan.csr import Skeleton as SkanSkeleton
+from skan.csr import summarize
+
+
+def image_to_graph_skan(skeleton_image: np.ndarray):
+    """Convert a skeleton image to a graph using skan."""
+    # make the skeleton
+    skeleton = SkanSkeleton(skeleton_image=skeleton_image)
+    summary_table = summarize(skeleton, separator="_")
+
+    # get all of the nodes
+    # this might be slow - may need to speed up
+    # source_nodes = set(summary_table["node_id_src"])
+    # destination_nodes = set(summary_table["node_id_dst"])
+    # all_nodes = source_nodes.union(destination_nodes)
+
+    skeleton_graph = nx.MultiGraph(
+        shape=skeleton.skeleton_shape, dtype=skeleton.skeleton_dtype
+    )
+    for row in summary_table.itertuples(name="Edge"):
+        # Iterate over the rows in the table.
+        # Each row is an edge in the graph
+        index = row.Index
+        i = row.node_id_src
+        j = row.node_id_dst
+        # Nodes are added if they don't exist so only need to add edges
+        skeleton_graph.add_edge(
+            i,
+            j,
+            **{
+                "path": skeleton.path_coordinates(index),
+            },
+        )
+    return skeleton_graph
